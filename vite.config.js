95cac292522fba8e38607b031e06d5ca17c18238
Alexei <alexeib505@gmail.com>
2026-03-23 22:14:46 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import viteImagemin from 'vite-plugin-imagemin';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	base: "/ceramic_soul/",
+	base: "/ceramic_soul-2/",
 	build: {
 		rollupOptions: {
 			input: {
